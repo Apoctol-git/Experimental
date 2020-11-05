d88a0f6b5864ad9a9ca96b4e8a60a5d98fa1e870
Apoctol-git <73225113+Apoctol-git@users.noreply.github.com>
2020-11-05 19:48:01 +0300

    Initial commit
    what done:
    Mouse click catching
    Create Mesh in click place
    Click create raycast
    Mesh change color once per 10 sec
    Mesh change color when raycast hited  him.
    
    First Part

---
+++ Assets/Script/BoardController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var coord = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit hit = new RaycastHit();
+        Ray ray = new Ray(new Vector3(coord.x, coord.y, 1), new Vector3(coord.x, coord.y, 10));
+        Physics.Raycast(ray, out hit);
+        if (hit.collider==null)
+        {
+            NewObject(coord);
+        }
+        else
+        {
+            var script = hit.collider.gameObject.GetComponent<GOScript>();
+            script.SwitchColor();
+        }
+    }
+    private void NewObject(Vector3 coord)
+    {
+        coord.z = 2;
+        CreateObject(PrimitiveType.Sphere, coord);
+    }
+    
+    private void CreateObject(PrimitiveType type, Vector3 coord)
+    {
+        var go = GameObject.CreatePrimitive(type);
+        go.AddComponent<GOScript>();
+        go.transform.position = coord;
+    }
+}
---
+++ Assets/Script/GOScript.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOScript : MonoBehaviour
+{
+    //private void OnMouseDown()
+    //{
+    //    var sr = gameObject.GetComponent<SpriteRenderer>();
+    //    sr.color = Color.green;
+    //}
+    Color[] TotalColors;
+    DateTime timeCreation;
+    private void Start()
+    {
+        TotalColors = new Color[] { Color.white, Color.grey, Color.red, Color.blue, Color.green, Color.cyan, Color.magenta, Color.yellow, Color.gray };
+        timeCreation = DateTime.Now;
+    }
+    private void Update()
+    {
+        var currentTimeSpan = DateTime.Now - timeCreation;
+        var conditionTimeSpan = new TimeSpan(0, 0, 10);
+        if (currentTimeSpan>=conditionTimeSpan)
+        {
+            timeCreation = DateTime.Now;
+            SwitchColor();
+        }
+    }
+    public void SwitchColor()
+    {
+        var color = GetRandomColor(gameObject.name);
+        gameObject.GetComponent<MeshRenderer>().material.color = color;
+    }
+    private Color GetRandomColor(string name)
+    {
+        if (name == "Sphere")
+        {
+            return TotalColors[UnityEngine.Random.Range(2, 5)];
+        }
+        return new Color(UnityEngine.Random.Range(0, 255), UnityEngine.Random.Range(0, 255), UnityEngine.Random.Range(0, 255));
+    }
+}
